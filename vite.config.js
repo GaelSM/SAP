1ef0de98315e3c77166babd9891583db074f2ffd
Segura Mendoza Gael Alejandro <108765188+GaelSM@users.noreply.github.com>
2023-12-02 23:09:07 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  bae: "https://gaelsm.github.io/SAP"
+  base: "https://gaelsm.github.io/SAP"
 })
